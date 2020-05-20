71d17b8014df1e126db5dc3238314291e4194fbe
Roice Johnson <rjohn230uncc.edu>
2020-05-19 23:25:42 -0400

    fixed again

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -7,6 +7,7 @@ import { SignupComponent } from './signup/signup.component';
 import { FourDigitCodeComponent } from './four-digit-code/four-digit-code.component';
 import { LayoutComponent } from './layout.component';
 import { LayoutRoutingModule } from './layout-routing.module';
+import { AuthGuard } from './auth.guard';
 
 
 
@@ -15,6 +16,7 @@ import { LayoutRoutingModule } from './layout-routing.module';
   imports: [
     CommonModule,
     LayoutRoutingModule
-  ]
+  ],
+  providers:[AuthGuard]
 })
 export class LayoutModule { }

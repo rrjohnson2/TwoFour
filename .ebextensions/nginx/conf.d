1fc6dce9eb330803a63ccff4c8bb5217e83d7189
Roice Johnson <rjohn230uncc.edu>
2020-06-01 19:16:20 -0400

    come here

--- .ebextensions/nginx/conf.d
+++ .ebextensions/nginx/conf.d
@@ -6,7 +6,4 @@ location / {
             proxy_set_header Host $host;
             proxy_cache_bypass $http_upgrade;
         }
-        location /auth {
-            proxy_pass http://127.0.0.1:8080/auth;
-
-        }
+        

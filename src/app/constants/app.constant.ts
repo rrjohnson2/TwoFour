081b5b5770f567f385311d2a34615a54db562726
Roice Johnson <rjohn230uncc.edu>
2020-06-01 23:27:53 -0400

    no message

--- src/app/constants/app.constant.ts
+++ src/app/constants/app.constant.ts
@@ -2,17 +2,17 @@
 
 export var isSmallScreen = window.screen.width < 992;
 
-export var backendUrl = "http://twofourbackend-env.eba-n4ismnaa.us-east-2.elasticbeanstalk.com/"
+export var backendUrl = "http://bakendtwofour-env.eba-2ezcpqfm.us-east-2.elasticbeanstalk.com/"
 
 export var image_server_url = "http://smsemail-env.eba-tbvf2xp5.us-east-2.elasticbeanstalk.com/"
 
 export enum Actions {
-    logOff,
-    login,
-    signup,
-    submit,
-    generateCode,
-    authenicateCode,
-    currrentContest,
-    previousContest
-  }
+  logOff,
+  login,
+  signup,
+  submit,
+  generateCode,
+  authenicateCode,
+  currrentContest,
+  previousContest
+}

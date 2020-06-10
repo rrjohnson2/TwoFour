49c12597f771dd18166b59eec76543498e043aaa
Roice Johnson <rjohn230uncc.edu>
2020-06-09 22:40:37 -0400

    fromat

--- src/app/constants/app.constant.ts
+++ src/app/constants/app.constant.ts
@@ -5,7 +5,7 @@ export var isSmallScreen = window.screen.width < 992;
 export var backendUrl = "http://bakendtwofour-env.eba-2ezcpqfm.us-east-2.elasticbeanstalk.com/"
 //export var backendUrl = "http://localhost:5000/"
 
-export var image_server_url = "http://twoforpicturefix-env.eba-2aehqx9u.us-east-2.elasticbeanstalk.com/" 
+export var image_server_url = "http://twoforpicturefix-env.eba-2aehqx9u.us-east-2.elasticbeanstalk.com/"
 //export var image_server_url = "http://localhost:8082/"
 
 export enum Actions {
